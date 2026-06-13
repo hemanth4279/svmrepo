9e929a83c8ccf6c0e3b86ac6ef4e90a213ccbf7a
Guduru Hemanth Humar Reddy <130274052+hemanth4279@users.noreply.github.com>
2026-06-13 23:29:07 +0530

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -48,7 +48,7 @@ const Home = ({ navigate }: HomeProps) => {
   const testimonials = [
     {
       name: 'K.UmaMaheswara Reddy',
-      role: 'CEO, Founder',
+      role: 'CEO, Founder of SVM ASSOCIATES',
       content: 'SVM Associates has been instrumental in managing our company finances. Their expertise in tax planning saved us significantly.',
     },
    
